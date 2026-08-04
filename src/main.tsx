124c7f0bf60997da5b5fdfc63205fdee5a69f540
mhollink <hollink.mh@gmail.com>
2026-08-04 10:20:34 +0200

    Amend the vite default scaffolding

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  console.log("No html element with id #root found!");
+  throw new Error("Root element not found!");
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
